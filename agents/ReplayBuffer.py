1a94dd588ef56a5b95535bbe4c332fbadcd42bcc
Prani Gopu <69959590+pranigopu@users.noreply.github.com>
2024-01-07 02:39:05 +0000

    Update comments in ReplayBuffer.py

--- agents/ReplayBuffer.py
+++ agents/ReplayBuffer.py
@@ -3,7 +3,7 @@ from collections import deque
 
 class ReplayBuffer:
     def __init__(self, buffer_size, random_state):
-        # Replay buffer data structure:
+        # replay buffer data structure
         self.buffer = deque(maxlen=buffer_size)
 
         self.random_state = random_state
@@ -17,7 +17,7 @@ class ReplayBuffer:
     def draw(self, batch_size):
         N = len(self.buffer)
 
-        # Randomly sampling `batch_size` buffer indices without replacement:
+        # randomly sampling `batch_size` buffer indices without replacement
         indicies = self.random_state.choice(N, size=batch_size, replace=False)
 
         # return transitions corresponding to above indices
